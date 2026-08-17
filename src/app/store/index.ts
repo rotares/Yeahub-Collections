c4bd713d0ea96fda4a53804de859ea763a0f8086
Prigoda Pavel <rotares@disroot.org>
2026-08-17 16:16:14 +0700

    feat(store): add typify selector, dispatch

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -5,4 +5,8 @@ export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;

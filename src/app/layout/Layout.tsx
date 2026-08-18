29fe0644ab3bb3d8ce79eff5d8a7fee82947fa76
Prigoda Pavel <rotares@disroot.org>
2026-08-18 14:04:14 +0700

    style(Layout): add styles for layout

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import styles from './Layout.module.css';
 
 export const Layout = () => {
   return (
     <>
       <header>header</header>
 
-      <main>
+      <main className={styles.container}>
         <Outlet />
       </main>
 

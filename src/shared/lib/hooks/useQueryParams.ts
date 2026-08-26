223c77832d5589a72fbcc25767a3e82d854b26af
Pavel Prigoda <rotares@disroot.org>
2026-08-26 17:12:13 +0300

    fix(url sync): fix url sync in filter hook

--- src/shared/lib/hooks/useQueryParams.ts
+++ src/shared/lib/hooks/useQueryParams.ts
@@ -7,7 +7,6 @@ export const useQueryParams = () => {
   const page = Number(searchParams.get('page')) || 1;
   const limit = Number(searchParams.get('limit')) || 10;
   const titleOrDescriptionSearch = searchParams.get('titleOrDescriptionSearch') || undefined;
-
   const isFree = searchParams.has('isFree') ? searchParams.get('isFree') === 'true' : undefined;
 
   const specializations = useMemo(() => {
@@ -18,6 +17,10 @@ export const useQueryParams = () => {
       .map(Number)
       .filter((val) => !isNaN(val));
   }, [searchParams]);
+
+  const resetQueryParams = useCallback(() => {
+    setSearchParams({});
+  }, [setSearchParams]);
 
   const setQueryParams = useCallback(
     (
@@ -56,18 +59,19 @@ export const useQueryParams = () => {
     [setSearchParams],
   );
 
-  const resetQueryParams = useCallback(() => {
-    setSearchParams({});
-  }, [setSearchParams]);
-
-  return {
-    params: {
+  const params = useMemo(
+    () => ({
       page,
       limit,
       titleOrDescriptionSearch,
       isFree,
       specializations,
-    },
+    }),
+    [limit, page, titleOrDescriptionSearch, isFree, specializations],
+  );
+
+  return {
+    params,
     setQueryParams,
     resetQueryParams,
   };
